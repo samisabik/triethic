f6f022094b35c286c80afbc9c7ab8f5998020264
Sami <sabik.sami@gmail.com>
2017-12-18 16:30:35 +0000

    change server

--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ import psycopg2
 import sys
 
 con = None
+exists = False
 
 app = Flask(__name__)
 
@@ -12,15 +13,24 @@ def add_message():
 
     ts = datetime.datetime.now().strftime("%d%m%y-%H:%M")
     content = request.get_json(silent=True)
+    device_ID = content['ID'].lower()
+    value = content['data']
     print "SENSOR ID : " + content['ID']
     print "SENSOR VALUE : " + content['data']
     print "TIME : " + str(ts)
     try:
      
-        con = psycopg2.connect(database='triethoic', user='admin', password='KrOQpkWVZeZPGF4O') 
+        con = psycopg2.connect(database='triethic', user='admin', password='KrOQpkWVZeZPGF4O')
         cur = con.cursor()
-        cur.execute("INSERT INTO " + content['ID'].lower() + "(data,ts) VALUES(" + content['data'] +",'"+ str(ts) +"')")
-        #cur.execute("INSERT INTO " + content['ID'].lower() + "(data) VALUES(" + content['data'] +")")
+        cur.execute("select exists(select relname from pg_class where relname='" + device_ID + "')")
+        exists = cur.fetchone()[0]
+        
+        if not exists:
+            print "Creating table for "+device_ID
+            cur.execute("CREATE TABLE "+device_ID+"(id SERIAL, ts TEXT, data INT)")
+            con.commit()
+        
+        cur.execute("INSERT INTO " + device_ID + "(ts,data) VALUES(" + value +",'"+ str(ts) +"')")
         con.commit()
 
     except psycopg2.DatabaseError, e:
--- server/test_SQL.py
+++ server/test_SQL.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -9,13 +10,18 @@ con = None
 exists = False
 
 try:
-     
-    con = psycopg2.connect(database='triethic', user='admin', password='KrOQpkWVZeZPGF4O') 
+
+    con = psycopg2.connect(database='triethic', user='admin', password='KrOQpkWVZeZPGF4O')
     cur = con.cursor()
-    table_str = "test"
-    cur.execute("select exists(select relname from pg_class where relname='" + table_str + "')")
+
+    device_ID = "XZ56GU"
+    cur.execute("select exists(select relname from pg_class where relname='" + device_ID.lower() + "')")
     exists = cur.fetchone()[0]
     print exists
+    if not exists:
+        print "Creating table for "+device_ID
+        cur.execute("CREATE TABLE "+device_ID+"(ID SERIAL, TS TEXT, VALUE INT)")
+        con.commit()
     cur.close()
 
 except psycopg2.Error as e:
