069a7028579fcab3ec4f2071c41bb86db5c5b7e4
Sami <sabik.sami@gmail.com>
2017-12-18 12:39:48 +0000

    first com

--- server/test_SQL.py
+++ server/test_SQL.py
@@ -10,12 +10,13 @@ exists = False
 
 try:
      
-    con = psycopg2.connect(database='sami', user='sami') 
+    con = psycopg2.connect(database='trie', user='admin_trie', password='bobcool87') 
     cur = con.cursor()
     table_str = "test"
     cur.execute("select exists(select relname from pg_class where relname='" + table_str + "')")
     exists = cur.fetchone()[0]
     print exists
     cur.close()
+
 except psycopg2.Error as e:
     print e
