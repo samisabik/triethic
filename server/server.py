c5287cbbecccef2fbdf26e78e0c5b71860bae922
Sami <sabik.sami@gmail.com>
2018-01-24 01:03:12 +0000

    push

--- server/server.py
+++ server/server.py
@@ -20,8 +20,6 @@ def read_data():
         cur = con.cursor()
 
         cur.execute("UPDATE device_list SET last_value = " + sensor_level + " WHERE device_id = '" + sensor_ID + "'")
-
-        cur.execute("INSERT INTO d_" + sensor_ID + "(ts,data,rssi) VALUES(" + ts +"," + sensor_level + "," + int(sensor_rssi) +")")
         con.commit()
 
     except psycopg2.DatabaseError, e:
